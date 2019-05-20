341bcbc9e84d41cb0340224ad9495ffeb1ac772a
Chris <chris.m.hein@gmail.com>
2019-05-20 07:30:50 -0400

    fix PORT issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app  = express();
 const path = require('path')
 const sendMail = require('./mail')
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 
 //Data parsing
