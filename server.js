34ff2519f920db7cf28964f6f8700ae6734ecf03
Chris <chris.m.hein@gmail.com>
2019-05-19 23:04:34 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app  = express();
+const path = require('path')
+const sendMail = require('./mail')
+
+const PORT = 8080;
+
+
+//Data parsing
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "assets")));
+
+
+app.post('/email', (req, res) => {
+    //send email
+    const {subject, email, text } = req.body
+    console.log(req.body)
+    sendMail(email, subject, text, function (err, data){
+        if (err) {
+            res.status(500).json({ message: "internal error"})
+        } else {
+             console.log('Email sent!')
+        }
+    });
+    res.json({message: 'Message received!'})
+})
+
+app.get(('/'), (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/contact', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+app.get('/hireme', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'hireme.html'));
+});
+app.get('/languages', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'languages.html'));
+});
+app.get('/portfolio', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'portfolio.html'));
+});
+app.get(('/*'), (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT,  () => {
+    console.log('Server is listening on Port ', PORT)
+})
